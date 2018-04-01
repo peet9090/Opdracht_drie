6081c795ae230f8631e3bff2a4905e2d213b8562
peet9090 <33264912+peet9090@users.noreply.github.com>
2018-04-01 17:58:23 +0200

    Opdracht3_Versie1.4
    Api data ophalen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
-//console.log("test1");
+console.log("test1");
 
 var myRequest = new XMLHttpRequest();
 myRequest.open('GET','http://dennistel.nl/movies');
 myRequest.onload = function() {
   console.log(myRequest.responseText);
+  var myData = myRequest.responseText;
+  console.log(myData[0]);
 };
 
 myRequest.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+//bronnen:
+//peet9090.github.io/Opdracht_drie/
+//dennistel.nl/movies
+//moodle.cmd.hva.nl/mod/page/view.php?id=21392
+//developer.mozilla.org/en-US/docs/Web/API/UIEvent
+//bokardo.com/principles-of-user-interface-design/
+//www.youtube.com/watch?v=rJesac0_Ftw
+//developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+//

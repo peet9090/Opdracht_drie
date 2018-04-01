e34553c50976357af16644d3a7aa70f0bb22d920
peet9090 <33264912+peet9090@users.noreply.github.com>
2018-04-01 23:26:25 +0200

    Opdracht3_Versie2.0
    HTML is geinjecteerd met Javascript. Scroll funcite werkt tot zekere mate. Api werkt goed. Content wordt geladen wanneer er wordt geklikt op button, tot nu toe goede prototype.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,68 @@
-console.log("test1");
+//console.log("test1");
+var filmsLijst = document.getElementById('films');
 
+//Eerste AJAX die geschreven is
 var myRequest = new XMLHttpRequest();
 myRequest.open('GET','http://dennistel.nl/movies');
 myRequest.onload = function() {
-  console.log(myRequest.responseText);
-  var myData = myRequest.responseText;
+  //console.log(myRequest.responseText);
+  var myData = JSON.parse(myRequest.responseText);
   console.log(myData[0]);
+  ophalenHTML(myData);
 };
-
 myRequest.send();
 
 
 
+//button die meer films gaat weergeven
+var btn = document.getElementById('btn');
+//spreekt ID aan in html
+btn.addEventListener("click", function(){
+  var myRequest = new XMLHttpRequest();
+  myRequest.open('GET','http://dennistel.nl/movies');
+  myRequest.onload = function() {
+    //console.log(myRequest.responseText);
+    var myData = JSON.parse(myRequest.responseText);
+    //console.log(myData[0]);
+    ophalenHTML(myData);
+  };
+  myRequest.send();
+});
+
+function ophalenHTML(data){
+//html toevoegen aan de html
+  var htmlStuk = "";
+
+  for (i = 1; i < data.length; i++) {
+    htmlStuk += "<span>" + data[i].release_date+ ".</span>";
+    htmlStuk += "<h3>" + data[i].title + ".</h3>";
+    htmlStuk += "<img src='" + data[i].cover + "'>" + ".</img>";
+//    console.log("test1");
+  }
+  filmsLijst.insertAdjacentHTML('beforeend', htmlStuk);
+}
 
 
 
 
+
+
+var scrollen = document.getElementById('films');
+//console.log("portfolio " + portfolio)
+
+window.addEventListener("scroll", function(){
+  //console.log("window: " + window.scrollY);
+  //console.log("portfolio: " + portfolio.offsetTop);
+
+	if(window.scrollY > scrollen.offsetTop - 100){
+    //console.log("test");
+    scrollen.classList.add('action');
+
+
+
+  };
+
+})
 
 
 
